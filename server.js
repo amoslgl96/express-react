84825faca10fc0968dca6b58ffa54ed160ff39db
amoslgl96 <baolgl96@gmail.com>
2019-10-02 14:16:21 +0800

    updated server to send build react file

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-const express = require('express');
-
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get('/sayHello', function(req, res){
-  res.send('Hello from the back-end.');
+const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, "client/build")));
+
+
+app.get("/sayHello", function(req, res) {
+  res.send("Hello from the back-end.");
 });
 
-app.listen(3000);
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+app.listen(port);
